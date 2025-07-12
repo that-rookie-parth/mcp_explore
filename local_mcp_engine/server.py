ea570cda228729491702a737fe96cc4ddc09cc91
Parth Kulshreshtha <kulshreshthaparth@gmail.com>
2025-07-12 23:48:53 +0530

    updated the code of the server to add the tools for the db

--- local_mcp_engine/server.py
+++ local_mcp_engine/server.py
@@ -1,17 +1,44 @@
 import logging
+from typing import Any, Dict, List
 
 from fastmcp import FastMCP
+
+from db import Database
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("fastmcp_server")
 
 mcp = FastMCP("My MCP Server")
+db: Database = Database()
 
 
 @mcp.tool
 def greet(name: str) -> str:
+    """
+    Returns a personalized greeting for the given name.
+    """
     logger.info("greet() tool called")
     return f"Hello {name}!!"
+
+
+@mcp.tool
+def add_person(name: str, age: int, profession: str) -> str:
+    """
+    Add a record of the person to the database.
+    """
+    logger.info("Adding a new record to db")
+    db.insert_person(name, age, profession)
+    return f"Record of {name} added successfully to the db."
+
+
+@mcp.tool
+def list_people() -> list[Any]:
+    """
+    Retrun the details about all the people stored in the db.
+    """
+    logging.info("Fetching the details of all the people from the db")
+    people: list[Any] = db.get_all_people()
+    return people
 
 
 app = mcp.http_app()
