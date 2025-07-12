0829d6a685519d6490663afac847152ace11ae53
Parth Kulshreshtha <kulshreshthaparth@gmail.com>
2025-07-12 22:03:23 +0530

    added the queries for the sqlite db

---
+++ local_mcp_engine/queries.sql
@@ -0,0 +1,14 @@
+-- name: create_table
+CREATE TABLE IF NOT EXISTS people (
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  name TEXT NOT NULL, age INTEGER NOT NULL, 
+  profession TEXT NOT NULL
+);
+
+-- name: insert_person
+INSERT INTO people (name, age, profession) 
+VALUES 
+  (:name, :age, :profession);
+
+-- name: get_all_people
+SELECT * FROM people;
